2d83127ff5ce01fe750add1c552632e345db5aba
Washington <104361028+WashingtonFranz@users.noreply.github.com>
2022-11-21 19:33:23 -0300

    Add files via upload

--- recuperação canvas/scripts.js
+++ recuperação canvas/scripts.js
@@ -17,36 +17,60 @@ const ctx = document.getElementById('exemplo').getContext('2d');    //contexto, 
  * 
  */
 
+//Gabarito A: 2 planetas ao redor do sol
+
+
 const centroX = 600;
 const centroY = 300;
-const orbt_sz = 200;        //tamanho da órbita da Terra
+const orbt_sz = 160;        //tamanho da órbita da Terra
+const orbm_sz = 220;        //tamanho da órbita da Terra
+const orbj_sz = 270;        //tamanho da órbita da jupiter
+const orbs_sz = 330;        //tamanho da órbita da saturno
 const orbl_sz = 30;         //tamanho da órbita da lua
+const orblm_sz = 30;        //tamanho da órbita da lua de marte
+const orblj_sz = 30;        //tamanho da órbita da lua de jupiter
+const orbls_sz = 30;        //tamanho da órbita da lua de saturno
+
 const sol_sz = 50;          //tamanho do sol
-const lua_sz = 10;           //tamanho da lua
+const lua_sz = 5;          //tamanho da lua
+const luam_sz = 4;         //tamanho da lua de marte
+const luaj_sz = 4;         //tamanho da lua de jupiter
+const luas_sz = 4;         //tamanho da órbita da lua de saturno
 const terra_sz = 15;        //tamanho da Terra
+const marte_sz = 12;
+const jupiter_sz = 12;
+const saturno_sz = 8;
 const tau = 2 * Math.PI;    //útil para cálculos de circunferência
 const t_terra = 30;         //segundos por volta da terra (máx 60!)
-const t_lua = 1;            //segundos por volta da lua (máx 60!)
-
-const orbt2_sz = 208;        //tamanho da órbita da Terra
-const orbt3_sz = 233;        //tamanho da órbita da Terra
-const orbt4_sz = 267;        //tamanho da órbita da Terra
-
+const t_lua = 3;            //segundos por volta da lua (máx 60!)
+const t_marte = 20;
+const t_jupiter = 15;
+const t_saturno = 10;
+const t_luam = 4;
+const t_luaj = 4;
+const t_luas = 4;
 
 const sol = new Path2D();
 const lua = new Path2D();
 const terra = new Path2D();
-const terra2 = new Path2D();
-const terra3 = new Path2D();
-const terra4 = new Path2D();
+const marte = new Path2D();
+const jupiter = new Path2D();
+const saturno = new Path2D();
+const luam = new Path2D();
+const luaj = new Path2D();
+const luas = new Path2D();
 
 function init() {
-    sol.arc(0,0,sol_sz,0,6.29,false);
-    lua.arc(0,1200,lua_sz,0,6.29,false);
-    terra.arc(0,0,terra_sz,0,6.29,false);
-    terra.arc(0,60,terra_sz,0,6.29,false);
-    terra.arc(0,120,terra_sz,0,6.29,false);
-    terra.arc(0,180,terra_sz,0,6.29,false);
+    sol.arc(0,0,sol_sz,0,tau,false);
+    lua.arc(0,0,lua_sz,0,tau,false);
+    terra.arc(0,0,terra_sz,0,tau,false);
+    luam.arc(0,0,luam_sz,0,tau,false);
+    luaj.arc(0,0,luaj_sz,0,tau,false);
+    marte.arc(0,0,marte_sz,0,tau,false);
+    jupiter.arc(0,0,jupiter_sz,0,tau,false);
+    saturno.arc(0,0,saturno_sz,0,tau,false);
+    luas.arc(0,0,luas_sz,0,tau,false);
+
     window.requestAnimationFrame(draw);
 }
 
@@ -55,47 +79,15 @@ function draw() {
     ctx.fillRect(0, 0, 1200, 600); // clear canvas
 
     ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-    ctx.save();
-
-        // terra
+    ctx.save(); // terra
         ctx.translate(centroX, centroY);    //centro do sol
         
         const time = new Date();
         ctx.rotate((tau/t_terra) * time.getSeconds() + (tau/(t_terra*1000)) * time.getMilliseconds());
-        ctx.translate(200, 0);      //distância da órbita ao sol
+        ctx.translate(orbt_sz, 0);      //distância da órbita ao sol
 
         ctx.fillStyle = '#7cf';
         ctx.fill(terra); 
-
-        // terra 2
-        ctx.translate(centroX, centroY);    //centro do sol
-        
-        const time2 = new Date();
-        ctx.rotate((tau/t_terra) * time.getSeconds() + (tau/(t_terra*1000)) * time.getMilliseconds());
-        ctx.translate(200, 0);      //distância da órbita ao sol
-
-        ctx.fillStyle = '#7cf';
-        ctx.fill(terra2); 
-
-         // terra 3
-         ctx.translate(centroX, centroY);    //centro do sol
-        
-         const time3 = new Date();
-         ctx.rotate((tau/t_terra) * time.getSeconds() + (tau/(t_terra*1000)) * time.getMilliseconds());
-         ctx.translate(200, 0);      //distância da órbita ao sol
- 
-         ctx.fillStyle = '#7cf';
-         ctx.fill(terra3); 
-
-        // terra 4
-        ctx.translate(centroX, centroY);    //centro do sol
-        
-        const time4 = new Date();
-        ctx.rotate((tau/t_terra) * time.getSeconds() + (tau/(t_terra*1000)) * time.getMilliseconds());
-        ctx.translate(200, 0);      //distância da órbita ao sol
-
-        ctx.fillStyle = '#7cf';
-        ctx.fill(terra4); 
 
         // lua
         ctx.save();
@@ -108,7 +100,7 @@ function draw() {
         
         // Desenho da Órbita da lua - pedir para alunos
         ctx.beginPath();
-        ctx.arc(0,0, orbl_sz, 0, tau, false);
+        ctx.arc(0, 0, orbl_sz, 0, tau, false);
         ctx.strokeStyle = '#fff4';
         ctx.stroke();
 
@@ -120,28 +112,118 @@ function draw() {
     ctx.strokeStyle = '#7cf6';
     ctx.stroke();
 
-    // Desenho da Órbita da terra 2
+    
+    //marte:
+
+    ctx.save();
+        ctx.translate(centroX, centroY);    //centro do sol
+        
+        ctx.rotate((tau/t_marte) * time.getSeconds() + (tau/(t_marte*1000)) * time.getMilliseconds());
+        ctx.translate(orbm_sz, 0);      //distância da órbita ao sol
+
+        ctx.fillStyle = 'red';
+        ctx.fill(marte); 
+
+        // lua
+        ctx.save();
+            ctx.rotate((tau/t_luam) * time.getSeconds() + (tau/(t_luam*1000)) * time.getMilliseconds());
+            ctx.translate(0, orblm_sz);
+            ctx.fillStyle = 'white';
+            ctx.fill(luam);
+        ctx.restore();  //restaura pré-lua
+        
+        // Desenho da Órbita da lua 
+        ctx.beginPath();
+        ctx.arc(0, 0, orblm_sz, 0, tau, false);
+        ctx.strokeStyle = '#fff4';
+        ctx.stroke();
+
+
+    ctx.restore();  //restaura pré-terra
+
+    // Desenho da Órbita da terra
     ctx.beginPath();
-    ctx.arc(centroX, centroY, orbt2_sz, 0, tau, false);
+    ctx.arc(centroX, centroY, orbm_sz, 0, tau, false);
     ctx.strokeStyle = '#7cf6';
     ctx.stroke();
 
-    // Desenho da Órbita da terra 3
-    ctx.beginPath();
-    ctx.arc(centroX, centroY, orbt3_sz, 0, tau, false);
-    ctx.strokeStyle = '#7cf6';
-    ctx.stroke();
+          //jupiter:
 
-    // Desenho da Órbita da terra 4
-    ctx.beginPath();
-    ctx.arc(centroX, centroY, orbt4_sz, 0, tau, false);
-    ctx.strokeStyle = '#7cf6';
-    ctx.stroke();
+    ctx.save();
+    ctx.translate(centroX, centroY);    //centro do sol
+    
+    ctx.rotate((tau/t_jupiter) * time.getSeconds() + (tau/(t_jupiter*1000)) * time.getMilliseconds());
+    ctx.translate(orbj_sz, 0);      //distância da órbita ao sol
+
+    ctx.fillStyle = '#5C4033';
+    ctx.fill(jupiter); 
+
+    // lua
+    ctx.save();
+        ctx.rotate((tau/t_luaj) * time.getSeconds() + (tau/(t_luaj*1000)) * time.getMilliseconds());
+        ctx.translate(0, orblj_sz);
+        ctx.fillStyle = 'white';
+        ctx.fill(luaj);
+     ctx.restore();  //restaura pré-lua
+    
+        // Desenho da Órbita da lua 
+        ctx.beginPath();
+        ctx.arc(0, 0, orblj_sz, 0, tau, false);
+        ctx.strokeStyle = '#fff4';
+        ctx.stroke();
+
+
+        ctx.restore();  //restaura pré-terra
+
+        // Desenho da Órbita de jupiter
+        ctx.beginPath();
+        ctx.arc(centroX, centroY, orbj_sz, 0, tau, false);
+        ctx.strokeStyle = '#7cf6';
+        ctx.stroke();
+  
+            ctx.restore();  //restaura pré-terra
+            
+        //saturno:
+
+        ctx.save();
+        ctx.translate(centroX, centroY);    //centro do sol
+        
+        ctx.rotate((tau/t_saturno) * time.getSeconds() + (tau/(t_saturno*1000)) * time.getMilliseconds());
+        ctx.translate(orbs_sz, 0);      //distância da órbita ao sol
+
+        ctx.fillStyle = '#ffff00';
+        ctx.fill(saturno); 
+
+        // lua
+        ctx.save();
+            ctx.rotate((tau/t_luas) * time.getSeconds() + (tau/(t_luas*1000)) * time.getMilliseconds());
+            ctx.translate(0, orbls_sz);
+            ctx.fillStyle = 'white';
+            ctx.fill(luas);
+        ctx.restore();  //restaura pré-lua
+        
+        // Desenho da Órbita da lua 
+        ctx.beginPath();
+        ctx.arc(0, 0, orbls_sz, 0, tau, false);
+        ctx.strokeStyle = '#fff4';
+        ctx.stroke();
+
+
+        ctx.restore();  //restaura pré-terra
+
+        // Desenho da Órbita da terra
+        ctx.beginPath();
+        ctx.arc(centroX, centroY, orbs_sz, 0, tau, false);
+        ctx.strokeStyle = '#7cf6';
+        ctx.stroke();
+
+
+
 
     // sol
     ctx.save();
         ctx.translate(centroX, centroY);
-        ctx.fillStyle = 'yellow';
+        ctx.fillStyle = 'orange';
         ctx.fill(sol);
         ctx.lineWidth = 4;
         ctx.strokeStyle = '#fd2';
